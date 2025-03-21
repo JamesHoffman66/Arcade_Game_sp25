d99ea18fe12c83cd65ac2395c242a2a2750ce8d3
JamesHoffman66 <jhoffm12@student.fitchburgstate.edu>
2025-03-20 21:41:43 -0400

    Basic Movement and enemy collision

--- Assets/Scripts/Player1Controller.cs
+++ Assets/Scripts/Player1Controller.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class Player1Controller : MonoBehaviour
 {
     public float speed = 10f;
+    public float jumpForce = 35f;
     public float horizontalInput;
     public Rigidbody rb;
+    public bool isOnGround;
+    public float gravityMod;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityMod;
     }
 
     // Update is called once per frame
@@ -19,9 +23,31 @@ public class Player1Controller : MonoBehaviour
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
 
-        if (Input.GetKeyDown(KeyCode.Space)) 
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround) 
         {
-            rb.AddForce(Vector3.up * 10, ForceMode.Impulse);
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
         }
+
+        
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+            Debug.Log("Grounded");
+        }
+        
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(gameObject);
+        }
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isOnGround = true;
+            Debug.Log("Grounded");
+        }
+
     }
 }
--- Assets/Scripts/Player2Controller.cs
+++ Assets/Scripts/Player2Controller.cs
@@ -7,10 +7,13 @@ public class Player2Controller : MonoBehaviour
     public float speed = 10f;
     public float VerticalInput;
     public Rigidbody rb2;
+    public bool isOnGround;
+   
     // Start is called before the first frame update
     void Start()
     {
         rb2 = GetComponent<Rigidbody>();
+        
     }
 
     // Update is called once per frame
@@ -19,9 +22,30 @@ public class Player2Controller : MonoBehaviour
         VerticalInput = Input.GetAxis("Vertical");
         transform.Translate(Vector3.right * VerticalInput * speed * Time.deltaTime);
 
-        if (Input.GetKeyDown(KeyCode.Alpha4)) 
+        if (Input.GetKeyDown(KeyCode.Alpha4) && isOnGround) 
         {
-            rb2.AddForce(Vector3.up * 10, ForceMode.Impulse);
+            rb2.AddForce(Vector3.up * 35, ForceMode.Impulse);
+            isOnGround = false;
         }
     }
-}
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+            Debug.Log("Grounded");
+        }
+
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(gameObject);
+        }
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            isOnGround = true;
+            Debug.Log("Grounded");
+        }
+        
+
+    }
+}   
