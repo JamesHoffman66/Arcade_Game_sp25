3962c159e8653cce602c279e3efb09b18abcba54
jmalagodi <jmalagod@student.fitchburgstate.edu>
2025-03-22 18:46:50 -0400

    combined level and new movement/AI

--- Assets/Scripts/EnemyAggroFixed.cs
+++ Assets/Scripts/EnemyAggroFixed.cs
@@ -1,16 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
 
-public class EnemyAggro : MonoBehaviour
+public class EnemyAggroFixed : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject pointA;
     public GameObject pointB;
     public GameObject player;
     public GameObject player2;
-    
+
     public Rigidbody rb;
     private Transform currentPoint;
     public float speed = 2.5f;
@@ -27,41 +26,22 @@ public class EnemyAggro : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector3 lookDirection = (player.transform.position - transform.position).normalized;
         Vector2 directionToPlayer = player.transform.position - transform.position;
         Vector2 directionToPlayer2 = player2.transform.position - transform.position;
 
+
         float angleToPlayer = Vector3.Angle(transform.right, directionToPlayer);
         float angleToPlayer2 = Vector3.Angle(transform.right, directionToPlayer2);
-        float distanceToPlayer = Vector2.Distance(transform.position, player.transform.position);
-        float distanceToPlayer2 = Vector2.Distance(transform.position, player2.transform.position);
-
-        GameObject closestPlayer = (distanceToPlayer < distanceToPlayer2) ? player : player2;
-
-        Vector3 lookDirection = (closestPlayer.transform.position - transform.position).normalized;
 
         bool playerInSight = angleToPlayer < fieldOfViewAngle / 2f;
         bool player2InSight = angleToPlayer2 < fieldOfViewAngle / 2f;
 
 
-        if (Vector2.Distance(transform.position, player.transform.position) < aggroRange && playerInSight || (Vector2.Distance(transform.position, player.transform.position) < aggroRange && playerInSight))
+        if (Vector2.Distance(transform.position, player.transform.position) < aggroRange && playerInSight || Vector2.Distance(transform.position, player2.transform.position) < aggroRange && player2InSight)
         {
             patrol = false;
-
         }
-        if (!patrol)
-        {
-            speed = 15;
-
-            rb.velocity = new Vector2(0, 0);
-            rb.AddForce(lookDirection * speed, ForceMode.Acceleration);
-
-            if (Vector2.Distance(transform.position, player.transform.position) > aggroRange && Vector2.Distance(transform.position, player2.transform.position) > aggroRange)
-            {
-                patrol = true;
-            }
-        }
-
-
 
         if (patrol)
         {
@@ -77,18 +57,50 @@ public class EnemyAggro : MonoBehaviour
             if (Vector2.Distance(transform.position, currentPoint.position) < 1f && currentPoint == pointB.transform)
             {
                 currentPoint = pointA.transform;
-                
+
                 transform.Rotate(0, 180, 0);
             }
             if (Vector2.Distance(transform.position, currentPoint.position) < 1f && currentPoint == pointA.transform)
             {
                 currentPoint = pointB.transform;
-                
+
                 transform.Rotate(0, 180, 0);
             }
         }
-        
-        
-        
+        else
+        {
+            speed = 15;
+
+            rb.velocity = new Vector2(0, 0);
+            rb.AddForce(lookDirection * speed, ForceMode.Acceleration);
+
+
+
+
+            if (transform.position.x > pointB.transform.position.x)
+            {
+
+                rb.velocity = new Vector2(0, 0);
+                rb.AddForce(lookDirection * -speed, ForceMode.Acceleration);
+                transform.position = new Vector3(pointB.transform.position.x, transform.position.y, transform.position.z);
+
+
+            }
+            if (transform.position.x < pointA.transform.position.x)
+            {
+
+                rb.velocity = new Vector2(0, 0);
+                rb.AddForce(lookDirection * -speed, ForceMode.Acceleration);
+                transform.position = new Vector3(pointA.transform.position.x, transform.position.y, transform.position.z);
+
+
+            }
+            if (Vector2.Distance(transform.position, player.transform.position) > aggroRange && Vector2.Distance(transform.position, player2.transform.position) > aggroRange)
+            {
+                patrol = true;
+            }
+
+        }
+
     }
 }
